76530e2f4f242d9123eb7cd467cd5f60fb269f78
Quy Truong <qmtruong2@gmail.com>
2019-01-02 13:28:21 -0800

    setting up project

---
+++ client/src/components/test.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Test extends Component {
+    async componentDidMount() {
+        const message = {
+            text: 'this is a message from the client, Hello!',
+            name: 'kate'
+        }
+
+        const resp = await axios.post('/api/send-message', message);
+
+        console.log('send message resp:', resp);
+        // const resp = await axios.get('/api/test');
+        //
+        // console.log('Test Response:', resp);
+        //
+        // const user = await axios.get('/api/user');
+        //
+        // console.log('user response:', user)
+    }
+    render() {
+        return (
+            <div>
+                <h1>Test Component</h1>
+            </div>
+        )
+    }
+}
+export default Test;
